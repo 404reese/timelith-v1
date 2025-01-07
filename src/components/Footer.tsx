8c98c2ec2b0b45b95be7a88e5c61e88551eafa60
Riddhesh <riddhesh.chaudhary@gmail.com>
2025-01-07 19:22:32 +0530

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer border-t border-gray-300">
+      <p>&copy; {new Date().getFullYear()} My App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
